75ac4bc221ccea1a63f00583fe16c99dd8ac4b27
Kamil-Jan <kamilyakubov80@gmail.com>
2021-01-12 15:40:46 +0300

    small changes

--- .config/scripts/copy_output.sh
+++ .config/scripts/copy_output.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 selected=$(cat ~/.cache/zsh/history | tail -100 | rofi -i -matching normal -dmenu -p "Select command")
-[[ -n $selected ]] && eval "$selected" | xclip -selection c
+[[ -n $selected ]] && (eval "$selected" | xclip -selection c)
 
